823a9cdc84c38f3b0561b808465d2f94a7282960
diansheng <chnlds@gmail.com>
2012-02-27 21:33:56 -0600

    Update src/EightPuzzle.java

--- src/EightPuzzle.java
+++ src/EightPuzzle.java
@@ -17,10 +17,12 @@ public class EightPuzzle {
 	public static final StateNode Goal= new StateNode (goal);
 	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//game starts, welcome to users
 		System.out.println("Welcome to world of 8 puzzle!");
 		System.out.println("Solving the puzzle below with the 0 indicating space");
 		System.out.println("Type in the block number you want to move at each step.");
+		
+		//generate an 8 puzzle question
 		StateNode sn = generator();
 		
 		Scanner scanner = new Scanner(System.in);
@@ -29,12 +31,14 @@ public class EightPuzzle {
 		
 		while(flag)
 		{
+		//print out current 8 puzzle
 		sn.print();
 		System.out.println("Type "+help+" to see solution."); 
+		//read in user's command
 		int nextmove= scanner.nextInt();
-		if (nextmove!=0)
+		if (nextmove!=0)//if user doesn't give up
 		{
-			operate(nextmove);
+			sn.operate(nextmove);
 			if (sn.rel==0)
 			{
 				flag=false;
@@ -59,14 +63,19 @@ public class EightPuzzle {
 	
 	public static StateNode generator()
 	{
-		StateNode sn;
-		//check if solvable by calculating the odd or even
-		if (solver.isSolvable(sn));
+		Random rgen = new Random();  // Random number generator
+		do{
+			//generate a shuffled array
+			int [] arr = {0,1,2,3,4,5,6,7,8};
+			for (int i=0; i<9; i++) {
+				int randomPosition = rgen.nextInt(9);
+				int temp = arr[i];
+				arr[i] = arr[randomPosition];
+				arr[randomPosition] = temp;				
+			}
+			//build up a statenode and return if solvable
+			StateNode sn=new StateNode(array);
+		}while(if (solver.isSolvable(sn)));
 		return sn;
 	}
-	
-	public static void operate(int nextmove){
-		//make movement according to user input
-	}
-	
 }
